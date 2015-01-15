f2d3be26e5974b31f16205806860f5800bfdec27
Ajay Ramesh <ajayramesh.dev@gmail.com>
2015-01-14 22:30:13 -0800

    fixed links and youtube embeds in chat

--- public/js/talk.js
+++ public/js/talk.js
@@ -4,7 +4,7 @@
 
 */
 
-var STTalk = angular.module('STTalk',[]);
+var STTalk = angular.module('STTalk',['ngSanitize']);
 
 STTalk.controller('TalkController',function($scope, $http, $timeout){
 	$scope.token = token;
@@ -47,8 +47,9 @@ STTalk.controller('TalkController',function($scope, $http, $timeout){
 		query: 'token='+token+'&roomName='+$scope.roomName+'&roomId='+$scope.roomId
 	});
 
-	window.onUnload = function(){
+	window.onbeforeunload = function(){
 		socket.disconnect();
+		return "You are now disconnecting from this room";
 	}
 
 	if(socket && $scope.isMute==false){
@@ -109,43 +110,58 @@ STTalk.directive('ngEnter', function () {
     };
 });
 
-STTalk.directive('message',function(){
+STTalk.directive('message',function($compile){
+
 	return {
 		restrict: 'E',
 		transclude: true,
 		replace: true,
 		scope: {
-			message: '=message'
+			message: '=message',
+			index: '=index'
 		},
+		transclude: true,
 		link: function(scope,element,attrs){
-
+			
 			var dateString = moment(scope.message.sendTime).fromNow();
 			scope.message.sendTime = dateString;
 			var content = scope.message.content;
 			var imageMatches = content.match(/(http:\/\/[\w\-\.]+\.[a-zA-Z]{2,3}(?:\/\S*)?(?:[\w])+\.(?:jpg|png|gif|jpeg|bmp))/ig);
 			var urlMatches = content.match(/https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,}/ig);
 			var desmosMatches = content.match(/https?:\/\/www.desmos.com\/calculator\/[a-zA-Z0-9]+/ig);
-			var youtubeMatches = content.match(/(?:.+?)?(?:\/v\/|watch\/|\?v=|\&v=|youtu\.be\/|\/v=|^youtu\.be\/)([a-zA-Z0-9_-]{11})+/ig)
+			var youtubeMatches = content.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g);
 			if(imageMatches){
 			 	for(var i=0; i<imageMatches.length; i++){
-			 		scope.message.content = scope.message.content.replace(imageMatches[i],"");
+			 		content = content.replace(imageMatches[i],"");
 			 		element.append("<img class='talk-message-image' src='"+imageMatches[i]+"'></br>");
 			 	}
 			}
 			if(desmosMatches){
 				for(var d=0; d<desmosMatches.length; d++){
-					scope.message.content = scope.message.content.replace(desmosMatches[d],"");
+					content = content.replace(desmosMatches[d],"");
 					element.append("<iframe class='talk-message-desmos' src='"+desmosMatches[d]+"'>");
 				}
 			}
+			/* http://stackoverflow.com/a/3452617/896112 */
 			if(youtubeMatches){
 				for(var y=0; y<youtubeMatches.length; y++){
-					scope.message.content = scope.message.content.replace(youtubeMatches[y],"");
-					var video_id = youtubeMatches[y].match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/)[7];
-					element.append("<iframe class='talk-message-youtube' width='560' height='315' src='http://www.youtube.com/embed/"+video_id+"' frameborder='0' allowfullscreen></iframe>");
+					content = content.replace(youtubeMatches[y],"");
+					var video_id = youtubeMatches[y].split('v=')[1];
+					var ampersandPosition = video_id.indexOf('&');
+					if(ampersandPosition != -1) {
+					  video_id = video_id.substring(0, ampersandPosition);
+					}
+					element.append("<iframe class='talk-message-youtube' width='560' height='315' src='http://www.youtube.com/embed/"+video_id+"'frameborder='0' allowfullscreen></iframe>");
 				}
 			}
+			if(urlMatches){
+				for(var u=0; u<urlMatches.length; u++){
+					content = content.replace(urlMatches[u],"<a target='_blank' href="+urlMatches[u]+">"+urlMatches[u]+"</a>");
+				}
+			}
+			scope.message.content = content;
+
  		},
-		template:'<div><b class="talk-message-title">{{ message.sender.displayName }}</b><span class="talk-message-time">{{ message.sendTime }}</span><p class="talk-message-content">{{ message.content }}</p></div>',
+		template:"<div><b class='talk-message-title'>{{ message.sender.displayName }}</b><span class='talk-message-time'>{{ message.sendTime }}</span><p class='talk-message-content' ng-bind-html='message.content'>{{ message.content }}</p></div>",
 	};
 });
